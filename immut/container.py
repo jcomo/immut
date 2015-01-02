f3f25df5067f99302c3e857a3d585947fc3d124d
Jonathan Como <jonathan.como@gmail.com>
2015-01-02 02:45:32 -0500

    implement immutable container

--- immut/container.py
+++ immut/container.py
@@ -1,20 +1,56 @@
-"""
-example usage:
+class _ImmutableContainerType(type):
+    def __new__(mcs, name, bases, dct):
+        return super(_ImmutableContainerType, mcs).__new__(mcs, name, bases, dct)
 
-RequestModel = ImmutableContainer('signature', 'user_id')
+    def __init__(cls, name, bases, dct):
+        super(_ImmutableContainerType, cls).__init__(name, bases, dct)
+        cls._attributes = dct.get('attributes', [])
+        cls.__init__ = cls.__make_initializer()
+        cls.__setattr__ = cls.__make_setattr()
 
-u = User('jonathan')
-rm = RequestModel('test_signature', u)
-print rm.signature  # prints 'test_signature'
-rm.signature = 'another_signature'  # raises AttributeError
-print rm.password  # raises AttributeError
+    def __make_initializer(cls):
+        def initializer(self, *args, **kwargs):
+            if any(attr not in cls._attributes for attr in kwargs.keys()):
+                raise ValueError("Unknown attributes specified in constructor for class {}".format(cls.__name__))
+            for attr in cls._attributes:
+                value = kwargs.get(attr, None)  # TODO: this can be parametrized for custom default values
+                self.__dict__[attr] = value
+        return initializer
 
-ResponseModel = ImmutableContainer('message', 'code', builder=True)
-builder = ResponseModel()
-response = builder.message('hi').status(200).build()
+    def __make_setattr(cls):
+        def setter(self, name, value):
+            if name in cls._attributes:
+                raise AttributeError("Property {} is immutable".format(name))
+            super(cls, self).__setattr__(name, value)
+        return setter
 
-print response.message  # prints 'hi'
-response.message = 'hey'  # raises AttributeError
 
+class _ImmutableContainer(object):
+    """
+    example usage:
 
-"""
+    RequestModel = ImmutableContainer('signature', 'user_id')
+
+    u = User('jonathan')
+    rm = RequestModel('test_signature', u)
+    print rm.signature  # prints 'test_signature'
+    rm.signature = 'another_signature'  # raises AttributeError
+    print rm.password  # raises AttributeError
+
+    ResponseModel = ImmutableContainer('message', 'code', builder=True)
+    builder = ResponseModel()
+    response = builder.message('hi').status(200).build()
+
+    print response.message  # prints 'hi'
+    response.message = 'hey'  # raises AttributeError
+
+    # Or, without specifying all parameters
+    response = builder.message('hi').build()
+    response.status  # returns None
+    """
+    __metaclass__ = _ImmutableContainerType
+
+    def __call__(self, name, *args, **kwargs):
+        return _ImmutableContainerType(name, (object,), dict(attributes=[attr for attr in args]))
+
+ImmutableContainer = _ImmutableContainer()
--- immut/tests/test_container.py
+++ immut/tests/test_container.py
@@ -1,6 +1,62 @@
 import unittest
+from immut.container import ImmutableContainer
 
 
 class ContainerTestCase(unittest.TestCase):
-    def test_basic(self):
-        self.assertTrue(False)
+    def test_empty_container(self):
+        Container = ImmutableContainer('Container')
+        c = Container()
+        self.assertRaises(AttributeError, getattr, c, 'something')
+
+    def test_container_with_one_attribute(self):
+        Container = ImmutableContainer('Container', 'message')
+        c = Container(message='hello')
+        self.assertEquals(c.message, 'hello')
+
+    def test_container_raises_for_invalid_attribute(self):
+        Container = ImmutableContainer('Container', 'message')
+        self.assertRaises(ValueError, Container, message='hello', unspecified=True)
+
+    def test_container_cannot_set_values(self):
+        Container = ImmutableContainer('Container', 'message')
+        c = Container(message='hello')
+        self.assertRaises(AttributeError, setattr, c, 'message', 'hey')
+
+    def test_container_can_set_values_for_invalid_attributes(self):
+        Container = ImmutableContainer('Container', 'message')
+        c = Container()
+        try:
+            c.unspecified = True
+        except (AttributeError, ValueError, KeyError):
+            self.fail("Should be able to set unspecified properties")
+
+    def test_container_with_multiple_values(self):
+        Container = ImmutableContainer('Container', 'message', 'status', 'user')
+        message = 'success'
+        status = 0
+        user = type('User', (object,), dict())
+
+        c = Container(message=message, status=status, user=user)
+        self.assertEquals(c.message, message)
+        self.assertEquals(c.status, status)
+        self.assertEquals(c.user, user)
+
+    def test_container_with_multiple_values_optional(self):
+        Container = ImmutableContainer('Container', 'message', 'status', 'user')
+        message = 'success'
+        user = type('User', (object,), dict())
+
+        c = Container(message=message, user=user)
+        self.assertEquals(c.message, message)
+        self.assertEquals(c.user, user)
+        self.assertEquals(c.status, None)
+
+    def test_bad_class_names_dont_collide(self):
+        Container1 = ImmutableContainer('Container', 'message')
+        Container2 = ImmutableContainer('Container')
+
+        c1 = Container1(message='hi')
+        c2 = Container2()
+
+        self.assertEquals(c1.message, 'hi')
+        self.assertRaises(AttributeError, getattr, c2, 'message')
