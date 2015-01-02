3d845340a818b11e2af80102c94e8d178ad13a8c
Jonathan Como <jonathan.como@gmail.com>
2015-01-02 16:36:14 -0500

    doc strings

--- immut/container.py
+++ immut/container.py
@@ -1,4 +1,8 @@
 class _ImmutableContainerType(type):
+    """
+    Metaclass for immutable container. Creating an immutable container class gives modified init and setattr
+    methods. It is meant to be directly created; setting this as __metaclass__ will result in undefined behavior.
+    """
     def __init__(cls, name, bases, dct):
         super(_ImmutableContainerType, cls).__init__(name, bases, dct)
         attributes = dct.get('attributes', [])
@@ -7,6 +11,13 @@ class _ImmutableContainerType(type):
 
     @classmethod
     def make_initializer(mcs, attributes):
+        """
+        Create the init method for the class
+
+        :param attributes: the list of attributes (strings) to accept in the constructor as keyword args
+        :return: a modified version of the init method that will accept any keyword args so long as they
+        were specified in the list of attributes
+        """
         def initializer(self, *_, **kwargs):
             if any(attr not in attributes for attr in kwargs.keys()):
                 raise ValueError("Unknown attributes specified for class {}".format(self.__class__.__name__))
@@ -17,6 +28,13 @@ class _ImmutableContainerType(type):
 
     @classmethod
     def make_setattr(mcs, attributes):
+        """
+        Create the setattr method for the class. This version of setattr will raise an AttributeError when trying
+        to set any of the specified immutable attributes
+
+        :param attributes: a list of attributes (strings) to make immutable
+        :return: a modified version of the setattr method that keeps the specified attributes immutable
+        """
         def setter(self, name, value):
             if name in attributes:
                 raise AttributeError("Property {} is immutable".format(name))
@@ -25,6 +43,15 @@ class _ImmutableContainerType(type):
 
 
 def _make_container(container_name, *attributes):
+    """
+    Create a container class.
+
+    :param container_name: the name of the container class
+    :type container_name: basestring
+    :param attributes: strings, each representing a property on instances of the container class
+    :type attributes: basestring
+    :return: An immutable container class with the specified attributes
+    """
     if not isinstance(container_name, basestring):
         raise TypeError("Container name must be a string")
     if not container_name:
